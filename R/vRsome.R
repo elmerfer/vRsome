4549cc0f56e8012f74024e86b81bc7d6e540f142
elmerfer <elmerfer@gmail.com>
2024-04-04 10:21:12 -0300

    update

---
+++ R/vRsome.R
@@ -0,0 +1,31 @@
+#' BuildVarsomeVCF
+#' It buils the appropriate VFC file to feed Varsome Clinical
+#' from an MODApy excel panel
+#' @param xlsxFile the full file path
+#' @return it creates a VCF with the same name as the original file 
+#' @export
+#' 
+BuildVarsomeVCF <- function(xlsxFile){
+  if(!file.exists(xlsxFile)){
+    stop("file odes not exists")
+  }
+  panel <- openxlsx::read.xlsx(xlsxFile,sheet=3)
+  ## prepare the VCF header
+  
+  panel$QUAL="."
+  panel$FILTER="PASS"
+  panel$INFO="."
+  vcf.header <- c("##fileformat=VCFv4.2",
+                  paste0("##fileDate=",Sys.time()),
+                  "##source=vRsome",
+                  paste0("##reference=",xlsxFile),
+                  paste0(c("#CHROM", "POS", "ID","REF","ALT","QUAL","FILTER","INFO"),collapse = "\t"))
+  
+  vcf.header <- c(vcf.header,apply(panel[,c("CHROM","POS","RSID","REF","ALT","QUAL","FILTER","INFO")],MARGIN=1, FUN=function(x) paste0(x,collapse = "\t")))
+  writeLines(vcf.header,stringr::str_replace_all(xlsxFile,".xlsx","vRsome.vcf"))
+  if(file.exists(stringr::str_replace_all(xlsxFile,".xlsx","vRsome.vcf"))){
+    cat(paste0("\nSaved as: ",stringr::str_replace_all(xlsxFile,".xlsx","vRsome.vcf")))
+  }else{
+    stop("failed")
+  }
+}
